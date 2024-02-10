acfdce28a6719f683a1c809ecbe13954cf64f672
Toukach Mikhail <mikhail2.ru@mail.ru>
2024-02-10 23:17:06 +0300

    "сделал filterchain"

--- src/main/java/com/mikhailtoukach/spring/springeshop/config/SecurityConfig.java
+++ src/main/java/com/mikhailtoukach/spring/springeshop/config/SecurityConfig.java
@@ -9,27 +9,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import static org.apache.catalina.webresources.TomcatURLStreamHandlerFactory.disable;
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
-//        extends WebSecurityConfigurerAdapter {
 
-    private UserService userService;
+   private UserService userService;
 
     @Autowired
     public void setUserService(UserService userService) {
@@ -54,22 +47,21 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-   @Bean
-    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-       http.authorizeHttpRequests(request->request
-               .requestMatchers("/users/new").hasAuthority(Role.ADMIN.name())
-                .anyRequest().permitAll()
-                      .requestMatchers("/login").permitAll()
-                      .requestMatchers("/auth").permitAll());
-//               .logout()
-//               .logoutSuccessUrl("/logout")
-//                .logoutSuccessUrl("/").deleteCookies("JSESSIONID")
-//                .invalidateHttpSession(true)
-//               .disable();
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/users/new").hasAuthority(Role.ADMIN.name())
+                                .anyRequest()
+                                .permitAll())
+                                .formLogin((form)->form.loginPage("/login").permitAll()
+                                .loginProcessingUrl("/auth")
+                                .permitAll())
+                .logout((logout) -> logout.logoutUrl("/logout").logoutSuccessUrl("/").permitAll()
+                        .deleteCookies("JSESSIONID"));
         return http.build();
     }
-
 
 
 //    @Override
@@ -85,7 +77,7 @@ public class SecurityConfig {
 //                .permitAll()
 //                .and()
 //                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-//                .logoutSuccessUrl("/").deleteCookies("JSESSION10")
+//                .logoutSuccessUrl("/").deleteCookies("JSESSIONID")
 //                .invalidateHttpSession(true)
 //                .and()
 //                .csrf().disable();
